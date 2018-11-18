34bb38e47b467e116075b38bacc5e5f4ccbab0ef
Geonwoo-Jeong <geonwoo.jeong@gmail.com>
2018-11-18 15:42:41 +0900

    Service Workers part Two

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,1 +1,16 @@
-console.log("ServiceWorker is Working...");
+self.addEventListener("install", event => {
+  const offlinePage = new Request("/");
+  event.waitUntil(
+    fetch(offlinePage).then(response =>
+      caches.open("store").then(cache => cache.put(offlinePage, response))
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(error =>
+      caches.open("store").then(cache => cache.match("/"))
+    )
+  );
+});
